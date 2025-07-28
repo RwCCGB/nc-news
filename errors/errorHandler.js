ecf6a1a6118b05c86c18880697b35248caf05fa5
Matt Norris <rwcc@hotmail.co.uk>
2025-07-28 09:00:33 +0100

    Added two new error codes for timeout and undefined table (SQL)

--- errors/errorHandler.js
+++ errors/errorHandler.js
@@ -11,6 +11,12 @@ const handleDbError = (err, req, res, next) =>{
     if(err.code === "22P02"){
         res.status(400).send({msg: "Bad Request"})
     }
+    else if(err.code === "42P01"){
+        res.status(404).send({msg:"Table Not Found"})
+    }
+    else if(err.code === "57P05"){
+        res.status().send({msg:"Time Out"})
+    }
     else{
         res.status(500).send({msg:"Internal Server Error SQL"})
     }
