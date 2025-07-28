5f21c538b3ea7f33116ee7be07a3b9eeb7c95e9e
Matt Norris <rwcc@hotmail.co.uk>
2025-07-28 16:54:01 +0100

    SQL tidyup

--- models/comments.models.js
+++ models/comments.models.js
@@ -6,11 +6,24 @@ const fetchCommentsByArticleId = (article_id) =>{
         return Promise.reject({status: 400, msg: "Invalid article id"})
     }
     return db.query(
-        `SELECT articles.article_id, comments.comment_id, comments.votes, comments.created_at,  comments.author, comments.body, comments.article_id
-        FROM articles articles LEFT JOIN comments comments
-        ON articles.article_id = comments.article_id
-        WHERE articles.article_id = $1
-        ORDER BY comments.created_at 
+        `SELECT 
+            articles.article_id, 
+            comments.comment_id, 
+            comments.votes, 
+            comments.created_at,  
+            comments.author, 
+            comments.body, 
+            comments.article_id
+        FROM 
+            articles articles 
+        LEFT JOIN 
+            comments comments
+        ON 
+            articles.article_id = comments.article_id
+        WHERE 
+            articles.article_id = $1
+        ORDER BY 
+            comments.created_at 
         DESC`, [article_id]
     ).then(({rows}) => {
         if(rows.length === 0 || rows[0].article_id === null){
@@ -20,4 +33,5 @@ const fetchCommentsByArticleId = (article_id) =>{
         return allComments
     })
 }
+
 module.exports = fetchCommentsByArticleId
