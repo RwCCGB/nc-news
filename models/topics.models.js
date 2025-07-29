c9ea41ff13d9cc631e596d5d53c9041e9c3ac3f7
Matt Norris <rwcc@hotmail.co.uk>
2025-07-29 02:03:28 +0100

    Code clean up

--- models/topics.models.js
+++ models/topics.models.js
@@ -2,7 +2,6 @@ const db = require("../db/connection")
 
 const fetchTopics = () => {
     return db.query(`SELECT * FROM topics`).then(({rows}) => {
-        console.log(rows.length, "<< Total Rows")
         return rows
     })
 }
