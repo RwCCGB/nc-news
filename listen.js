606c8aa352edea210947d828343767578d2eb4c1
Matt Norris <rwcc@hotmail.co.uk>
2025-07-29 09:10:13 +0100

    Added DB settings for Production

--- listen.js
+++ listen.js
@@ -1,5 +1,9 @@
 const app = require('./app.js');
 
+const {PORT = 9090} = process.env
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`))
+/*
 app.listen(9090, (err) => { 
     if (err) {
         console.log(`Error:`, err);
@@ -7,3 +11,4 @@ app.listen(9090, (err) => {
         console.log(`listening on 9090`);
     }
 })
+    */
