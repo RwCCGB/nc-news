91761a6cf12002e6dd6e416d307b1ad37eef3760
Matt Norris <rwcc@hotmail.co.uk>
2025-08-11 11:28:58 +0100

    Allow CORS sharing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 
 app.use('/api',express.static(path.join(__dirname, 'public')))
