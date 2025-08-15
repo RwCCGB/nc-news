919d5baa11d894812b416b5138425d77bd534f17
Matt Norris <rwcc@hotmail.co.uk>
2025-08-15 22:16:36 +0100

    Update DB connection to attempt to bypass issues syncing Netlify to the backend

--- db/connection.js
+++ db/connection.js
@@ -4,14 +4,17 @@ const config = {}
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-
+let pool;
 if(ENV === "production"){
-  config.connectionString = process.env.DATABASE_URL
-  config.max = 2
+  pool = new Pool({connectionString: ENV, ssl: { rejectUnauthorized: false}, max: 1, idleTimeoutMillis: 30000})
+
+}
+else{
+  pool = new Pool(config)
 }
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool(config);
+module.exports = pool
